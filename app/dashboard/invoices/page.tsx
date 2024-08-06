3b0fb12a6a42d489ec998ea908d32b4659c5ab5a
Dimpfe <dimasprasetyo7696@gmail.com>
2024-08-06 21:58:06 +0700

    feat<>: add pagination and change to default export

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 interface SearchParams {
    searchParams?: {
@@ -13,9 +14,10 @@ interface SearchParams {
    }
 } 
 
-export default function Page({ searchParams }: SearchParams) {
+export default async function Page({ searchParams }: SearchParams) {
    const query = searchParams?.query || '';
    const currentPage = Number(searchParams?.page) || 1;
+   const totalPages = await fetchInvoicesPages(query);
 
    return (
       <div className="w-full">
@@ -35,7 +37,7 @@ export default function Page({ searchParams }: SearchParams) {
             <Table query={query} currentPage={currentPage} />
          </Suspense>
          <div className="mt-5 flex w-full justify-center">
-            {/* <Pagination totalPages={totalPages} /> */}
+            <Pagination totalPages={totalPages} />
          </div>
       </div>
    )
